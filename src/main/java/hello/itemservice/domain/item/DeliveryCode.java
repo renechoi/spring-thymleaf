756b223671ea3038b79f4c3c7da777b6f4e14979
renechoi <renechoi90@gmail.com>
2023-03-03 00:12:02 +0900

    Initial commit

---
+++ src/main/java/hello/itemservice/domain/item/DeliveryCode.java
@@ -0,0 +1,17 @@
+package hello.itemservice.domain.item;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * 빠른, 일반, 느린 배송
+ */
+
+@Data
+@AllArgsConstructor
+public class DeliveryCode {
+
+    private String code;
+    private String displayName;
+
+}
